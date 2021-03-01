4117f47ef13c9b1bdc18faa2ce8ea39a29676c7c
Nadine0109 <nadine0109@ya.ru>
2021-03-01 17:12:10 +0500

    New methods added

--- src/test/java/ru/netology/spotbugstry/SpotBugsStatisticsTest.java
+++ src/test/java/ru/netology/spotbugstry/SpotBugsStatisticsTest.java
@@ -8,13 +8,38 @@ class SpotBugsStatisticsTest {
 
     @Test
     void findMax() {
+        SpotBugsStatistics service = new SpotBugsStatistics();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateSum() {
+        SpotBugsStatistics service = new SpotBugsStatistics();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 85;
+
+        long actual = service.calculateSum(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findAverageSum() {
             SpotBugsStatistics service = new SpotBugsStatistics();
 
             long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-            long expected = 12;
+            long expected = 7;
 
-            long actual = service.findMax(incomesInBillions);
+            long actual = service.findAverageSum(incomesInBillions);
 
             assertEquals(expected, actual);
         }
-    }
+}
